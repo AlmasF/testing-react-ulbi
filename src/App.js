cd660de7ffc93b3bc7c21dae04659613926851bb
AlmasOnMac <abdugalievalmas@gmail.com>
2023-04-10 14:24:32 +0600

    add: router test

--- src/App.js
+++ src/App.js
@@ -1,43 +1,66 @@
-import { useState, useEffect } from 'react';
-import './App.css';
+// import { useState, useEffect } from 'react';
+// import './App.css';
 
-function App() {
-  const [data, setData] = useState(null);
-  const [toggle, setToggle] = useState(false);
-  const [value, setValue] = useState('');
+// function App() {
+//   const [data, setData] = useState(null);
+//   const [toggle, setToggle] = useState(false);
+//   const [value, setValue] = useState('');
 
-  const onClick = () => setToggle(prev => !prev);
+//   const onClick = () => setToggle(prev => !prev);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setData({});
-    }, 100);
-  }, []);
+//   useEffect(() => {
+//     setTimeout(() => {
+//       setData({});
+//     }, 100);
+//   }, []);
 
+//   return (
+//     <div className="App">
+//       { 
+//         toggle === true 
+//         && <div data-testId='toggle-elem'>toggle</div>
+//       }
+//       { 
+//         data && 
+//         <div style={{color: 'red'}}>data</div> 
+//       }
+//       <h1 data-testId="value-elem">{ value }</h1>
+//       <button
+//         data-testId="toggle-btn"
+//         onClick={onClick}
+//       >
+//         click me
+//       </button>
+//       <input
+//         type="text"
+//         placeholder="input value"
+//         onChange={e => setValue(e.target.value)}
+//       />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import MainPage from './pages/MainPage';
+import AboutPage from './pages/AboutPage';
+import ErrorPage from './pages/ErrorPage';
+
+const App = () => {
   return (
-    <div className="App">
-      { 
-        toggle === true 
-        && <div data-testId='toggle-elem'>toggle</div>
-      }
-      { 
-        data && 
-        <div style={{color: 'red'}}>data</div> 
-      }
-      <h1 data-testId="value-elem">{ value }</h1>
-      <button
-        data-testId="toggle-btn"
-        onClick={onClick}
-      >
-        click me
-      </button>
-      <input
-        type="text"
-        placeholder="input value"
-        onChange={e => setValue(e.target.value)}
-      />
+    <div>
+      <Link to="/" data-testid="main-link">main</Link>
+      <Link to="/about" data-testid="about-link">about</Link>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/*" element={<ErrorPage />} />
+      </Routes>
     </div>
-  );
-}
+  )
+};
 
 export default App;
---
+++ src/Router.test.js
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from './App';
+import { MemoryRouter } from "react-router-dom";
+
+describe('TEST APP', () => {
+    test('Router test', () => {
+        render(
+            <MemoryRouter>
+                <App />
+            </MemoryRouter>
+        );
+        const mainLink = screen.getByTestId('main-link');
+        const aboutLink = screen.getByTestId('about-link');
+        fireEvent.click(aboutLink);
+        expect(screen.getByTestId('about-page')).toBeInTheDocument();
+        fireEvent.click(mainLink);
+        expect(screen.getByTestId('main-page')).toBeInTheDocument();
+    });
+
+    test('Error test', () => {
+        render(
+            <MemoryRouter initialEntries={['/sasasa']}>
+                <App />
+            </MemoryRouter>
+        );
+        expect(screen.getByTestId('not-found-page')).toBeInTheDocument();
+        // const mainLink = screen.getByTestId('main-link');
+        // const aboutLink = screen.getByTestId('about-link');
+        // fireEvent.click(aboutLink);
+        // expect(screen.getByTestId('about-page')).toBeInTheDocument();
+        // fireEvent.click(mainLink);
+        // expect(screen.getByTestId('main-page')).toBeInTheDocument();
+    });
+});
