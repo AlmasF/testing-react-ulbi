7a8a7ba1c348f25522569bb62d029dee077de7af
AlmasOnMac <abdugalievalmas@gmail.com>
2023-04-10 13:01:43 +0600

    add: some snapshots and stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [toggle, setToggle] = useState(false);
+  const [value, setValue] = useState('');
+
+  const onClick = () => setToggle(prev => !prev);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setData({});
+    }, 100);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { 
+        toggle === true 
+        && <div data-testId='toggle-elem'>toggle</div>
+      }
+      { 
+        data && 
+        <div style={{color: 'red'}}>data</div> 
+      }
+      <h1 data-testId="value-elem">{ value }</h1>
+      <button
+        data-testId="toggle-btn"
+        onClick={onClick}
+      >
+        click me
+      </button>
+      <input
+        type="text"
+        placeholder="input value"
+        onChange={e => setValue(e.target.value)}
+      />
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,50 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('TEST APP', () => {
+  test('renders elements', () => {
+    render(<App />);
+    const btn = screen.getByRole('button');
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(btn).toBeInTheDocument();
+    expect(input).toBeInTheDocument();
+    expect(input).toMatchSnapshot();
+  });
+
+  test('renders learn react link', async () => {
+    render(<App />);
+    // const helloWorldElem = screen.queryByText(/hello2 world/i);
+    // expect(helloWorldElem).toBeNull();
+    screen.debug();
+    const helloWorldElem = await screen.findByText(/data/i);
+    expect(helloWorldElem).toBeInTheDocument();
+    expect(helloWorldElem).toHaveStyle({color: 'red'});
+    screen.debug();
+  });
+
+  test('CLICK EVENT', async () => {
+    render(<App />);
+    screen.debug();
+    const btn = screen.getByTestId('toggle-btn');
+    expect(screen.queryByTestId('toggle-elem')).toBeNull();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-elem')).toBeInTheDocument();
+    fireEvent.click(btn);
+    expect(screen.queryByTestId('toggle-elem')).toBeNull();
+  });
+
+  test('INPUT EVENT', async () => {
+    render(<App />);
+    screen.debug();
+    const input = screen.getByPlaceholderText(/input value/i);
+    expect(screen.queryByTestId('value-elem')).toContainHTML('');
+    // Искусственное событие
+    // fireEvent.input(input, {
+    //   target: { value: '123123' },
+    // });
+    // Близко к пользователям, обрабатываются события нажатия клавиш и т д
+    userEvent.type(input, '123123');
+    expect(screen.queryByTestId('value-elem')).toContainHTML('123123');
+  });
 });
